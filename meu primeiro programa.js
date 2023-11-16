97eebf649329799921221896dd797addb1154403
sofsty <150350677+sofsty@users.noreply.github.com>
2023-11-16 09:52:29 -0300

    meu primeiro programa.js

---
+++ meu primeiro programa.js
@@ -0,0 +1,18 @@
+# js.programacaocriativa.sofia
+function setup() {
+  createCanvas(600, 600);
+  background("violet")
+}
+
+
+function draw() {
+  
+   stroke("baby blue");
+  fill("cyan");
+
+//console.log (mouseIsPressed);
+
+if (mouseIsPressed); {
+  rect(mouseX, mouseY, 20, 35);
+}
+}
